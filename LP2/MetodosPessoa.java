2be359316616208bdb8f8cc8dc7d436dac2221d8
Datto06 <rafaosbcarvalho@gmail.com>
2026-08-04 14:12:53 -0300

    Adicionando primeiros trabalhos

---
+++ LP2/MetodosPessoa.java
@@ -0,0 +1,27 @@
+public interface MetodosPessoa{
+    public String get_nome();
+    public boolean set_nome(String nome);
+
+    public String get_sobrenome();
+    public boolean set_sobrenome(String sobrenome);
+
+    public String get_genero();
+    public boolean set_genero(String genero);
+
+    public LocalDate get_data_nascimento();
+    public boolean set_data_nascimento(LocalDate data);
+
+    public double get_peso();
+    public boolean set_peso(double peso);
+
+    public double get_altura();
+    public boolean set_altura(double altura);
+
+    public Pessoa get_pai();
+    public boolean set_pai(Pessoa pai);
+
+    public Pessoa get_mae();
+    public boolean get_mae(Pessoa mae);
+
+    private int idade();
+}
--- LP2/Pessoa.java
+++ LP2/Pessoa.java
@@ -1,40 +1,133 @@
 import java.time;
-interface MetodosPessoa{
-    public String get_nome();
-    public void set_nome(String nome);
-
-    public String get_sobrenome();
-    public void set_sobrenome(String sobrenome);
-
-    public String get_genero();
-    public void set_genero(String genero);
-
-    public LocalDate get_data_nascimento();
-    public void set_data_nascimento(LocalDate data);
-
-    public double get_peso();
-    public void set_peso(double peso);
-
-    public double get_altura();
-    public void set_altura(double altura);
-
-    public Pessoa get_pai();
-    public void set_pai(Pessoa pai);
-
-    public Pessoa get_mae();
-    public void get_mae(Pessoa mae);
-
-    private int idade();
-}
 
 public class Pessoa implements MetodosPessoa {
-    private String nome;
-    private String sobrenome;
+    private String nome,sobrenome;
     private String genero;
     private LocalDate data-nascimento;
-    private double peso;
-    private double altura;
-    private Pessoa pai;
-    private Pessoa mae;
-    public String get_nome();
+    private double peso,altura;
+    private Pessoa pai, mae; //posso fazer essa abreviacao
+
+    public String get_nome(){
+        return nome; //pode retornar nulo ou mal definido se ainda nao foi inicializado
+    }
+    public boolean set_nome(String nome){
+        if (nome!=null && nome.length()<=50){
+            this.nome=nome;
+            return true;
+        }
+        else if{nome==null}{
+            System.out.println("Erro, nome nulo vazio dado");
+            return false;
+        }
+        else if(nome.length()>50){
+            System.out.println("Erro, excedeu limite de caracteres");
+            return false;
+        }
+    }
+
+    public String get_sobrenome(){
+        return sobrenome; //pode retornar nulo ou mal definido se ainda nao foi inicializado
+    }
+    public boolean set_sobrenome(String sobrenome){
+        if (sobrenome!=null && sobrenome.length()<=50){
+            this.sobrenome=sobrenome;
+            return true;
+        }
+        else if{sobrenome==null}{
+            System.out.println("Erro, nome nulo dado");
+            return false;
+        }
+        else if(sobrenome.length()>50){
+            System.out.println("Erro, excedeu limite de caracteres");
+            return false;
+        }
+    }
+
+    public String get_genero(){
+        return genero; //pode retornar nulo ou mal definido se ainda nao foi inicializado
+    }
+    public boolean set_genero(String genero){ //Boolean da se executou com sucesso ou nao
+        if (genero!=null && genero.length()<=50){
+            this.genero=genero;
+        }
+        else if{genero==null}{
+            System.out.println("Erro, nome nulo dado.");
+            return false;
+        }
+        else if(genero.length()>50){
+            System.out.println("Erro, excedeu limite de caracteres");
+            return false;
+        }
+    }
+
+    public LocalDate get_data_nascimento(){
+
+    }
+    public boolean set_data_nascimento(LocalDate data){
+        if (data!=null){
+            this.data=data;
+            return true;
+        }
+        else{
+            System.out.println("Erro, data nula dada");
+            return false;
+        }
+    }
+
+    public double get_peso();
+    public boolean set_peso(double peso){
+        if (peso>0 && peso<=400){
+            this.peso=peso;
+            return true;
+        }
+        else if (peso<=0){
+            System.out.println("Erro, peso negativo ou zero.");
+            return false;
+        }
+        else if (peso>400){
+            System.out.println("Erro, peso acima de 400 kg, tem certeza desse valor?");
+            return false;
+        }
+    }
+
+    public double get_altura();
+    public boolean set_altura(double altura){
+        if (altura>0 && altura<=3){
+            this.altura=altura;
+            return true;
+        }
+        else if (altura<=0){
+            System.out.println("Erro, altura negativa ou zero");
+            return false;
+        }
+        else if (altura>3){
+            System.out.println("Erro, altura acima de 3 metros, tem certeza desse valor?");
+            return false;
+        }
+    }
+
+    public Pessoa get_pai();
+    public boolean set_pai(Pessoa pai){
+        if (pai!=null){
+            this.pai=pai;
+            return true;
+        }
+        else{
+            System.out.println("Erro, pai nulo dado");
+            return false;
+        }
+    }
+
+    public Pessoa get_mae();
+    public boolean get_mae(Pessoa mae){
+        if (mae!=null){
+            this.mae=mae;
+            return true;
+        }
+        else{
+            System.out.println("Erro, mae nulo dado");
+            return false;
+        }
+    }
+    private int idade();
 }
