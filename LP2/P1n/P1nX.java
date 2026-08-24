d64e127d11af38a263a71cfadb757f97beb26578
Datto06 <rafaosbcarvalho@gmail.com>
2026-08-23 21:22:57 -0300

    Adicinoando coisa basica

---
+++ LP2/P1n/P1nX.java
@@ -0,0 +1,7 @@
+public class P1nX{
+    public static void main(String[] args){
+        System.out.println(ValidaCPF.isCPF("123456ads78901    "));
+        System.out.println(ValidaCPF.isCPF("123456a78901"));
+
+    }
+}
---
+++ LP2/P1n/ValidaData.java
@@ -0,0 +1,27 @@
+public class ValidaData{
+    public static boolean isDia(int dia){
+
+    }
+    public static boolean isDia(String dia){
+
+    }
+    public static boolean isMes(int mes){
+
+    }
+    public static boolean isMes(String mes){
+
+    }
+    public static boolean isAno(int ano){
+
+    }
+    public static boolean isAno(String ano){
+
+    }    
+    public static boolean isDataValida(int data){
+
+    }
+    public static boolean isDataValida(String data){
+
+    }
+
+}
